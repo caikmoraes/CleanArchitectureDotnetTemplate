8d434b89004e2fed3fdbe233c13fa6294d1b1278
Caik de Moraes Souza <caikmoraes@pop-os.localdomain>
2023-11-10 13:28:10 -0300

    Fix dependency injection

--- Base.Clean.Template.Application/DependencyInjection.cs
+++ Base.Clean.Template.Application/DependencyInjection.cs
@@ -10,7 +10,7 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(typeof(DependencyInjection).Assembly);
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
 
         return services;
     }
